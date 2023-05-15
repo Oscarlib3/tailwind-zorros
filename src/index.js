b0f01a35931c13387ec5b1fc6e6789d01603d1eb
bdteo07814 <oscar.fermin@bt.gob.ve>
2023-05-15 10:48:09 -0400

    lazydom con nodo de unlist escuchar las imagenes por medio de clicks

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+import {registerImage} from './lazy'
+const minimo = 1;
+const maximo = 122;
 
-console.log('Happy hacking :)')
+const random = () => Math.floor(Math.random()*(maximo - minimo)) + minimo;
+const createImagen = () =>{
+    const container = document.createElement("div");
+    const imagen = document.createElement('img');
+    imagen.className = "mx-auto p-3" ;
+    imagen.width = "320";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+    container.appendChild(imagen)
+    return container;
+}
+
+const mountNode = document.getElementById("images");
+
+const botonImagen = document.getElementById("botonimagenes");
+
+const addImage = () =>{
+    const newImage = createImagen();
+    mountNode.appendChild(newImage);
+    registerImage(newImage);
+};
+botonImagen.addEventListener("click", addImage);
